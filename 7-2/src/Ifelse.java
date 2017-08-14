c4bc398675915c64b645ae276d01b1742924b542
YuChenXiao <31004702+YuChenXiao@users.noreply.github.com>
2017-08-14 23:31:41 +0800

    if else

---
+++ 7-2/src/Ifelse.java
@@ -0,0 +1,13 @@
+public class Ifelse {
+
+    public static void main (String[] args) {
+        int i = 10;
+        if (i > 10) {
+            System.out.println("大於10");
+        } else if (i < 10){
+        System.out.println("小於10");
+    }else{
+            System.out.println("等於10");
+        }   }
+
+}
