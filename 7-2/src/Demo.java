7addba83318e41ad5053833f24bc828f326e3e61
YuChenXiao <31004702+YuChenXiao@users.noreply.github.com>
2017-08-14 19:39:02 +0800

    99乘法

---
+++ 7-2/src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+
+    public static void main(String[] args){
+        for (int i = 1;i<10;i++) {
+            for (int u = 1 ;u<10;u++) {
+                System.out.print(u+"x"+i+"="+i*u + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
