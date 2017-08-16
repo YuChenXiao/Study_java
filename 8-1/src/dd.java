3ee3d9cecb138d1ac1b1ff6954ed4f0eb066b252
YuChenXiao <31004702+YuChenXiao@users.noreply.github.com>
2017-08-16 16:06:29 +0800

    0816

---
+++ 8-1/src/dd.java
@@ -0,0 +1,8 @@
+public class dd {
+    public static void main(String[] agrs){
+       for (int i = 1;i<7;i++){
+           print  ("*");
+       }
+    }
+
+}
---
+++ w/src/ww/st.java
@@ -0,0 +1,14 @@
+package ww;
+
+public class st {
+    public static void main(String[] args) {
+        for(int i = 5 ; i >0 ; i --) {
+            System.out.print(" ");
+            for(int j = 5; j >= i ; j --)
+                System.out.print("*");
+//            System.out.println();
+            //星星
+        }
+    }
+}
+
